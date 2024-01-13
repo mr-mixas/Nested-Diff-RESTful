01864a9eb4fdc19ec364995e05404df4c393877e
Michael Samoglyadov <mixas.sr@gmail.com>
2024-01-13 16:10:26 +0200

    add missing tests

--- tests/test_api_diff.py
+++ tests/test_api_diff.py
@@ -48,3 +48,29 @@ def test_ofmt_term(client):
     assert rv.status_code == 200
     assert rv.content_type.startswith('text/plain')
     assert rv.data == b"\x1b[32m+ 'b'\x1b[0m\n"
+
+
+def test_ofmt_html(client):
+    rv = client.post('/api/v1/diff', json={
+        'a': 'a',
+        'b': 'b',
+        'diff_opts': {'O': False},
+        'ofmt': 'html',
+    })
+
+    assert rv.status_code == 200
+    assert rv.content_type.startswith('text/plain')
+    assert rv.data == b'<div class="nDvD"><div>+ <div class="nDvN">&#x27;b&#x27;</div></div></div>'
+
+
+def test_ofmt_unsupported(client):
+    rv = client.post('/api/v1/diff', json={
+        'a': 'a',
+        'b': 'b',
+        'diff_opts': {'O': False},
+        'ofmt': '__unsupported__',
+    })
+
+    assert rv.status_code == 400
+    assert rv.content_type.startswith('text/html')
+    assert rv.data == b'Unsupported format __unsupported__'
